e4d4cc755925ddae0d644ac456dbacca49c43b26
Adam Bromiley <adam@bromiley.co.uk>
2020-06-07 22:00:35 +0100

    change colour scheme to enum

---
+++ include/colour.h
@@ -0,0 +1,15 @@
+#ifndef COLOUR_H
+#define COLOUR_H
+
+
+enum ColourScheme
+{
+    SCHEME_DEFAULT
+};
+
+
+extern const enum ColourScheme COLOUR_MIN;
+extern const enum ColourScheme COLOUR_MAX;
+
+
+#endif
