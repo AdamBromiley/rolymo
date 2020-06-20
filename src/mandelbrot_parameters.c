7f7ed1585c8810f81783aa5bbf0642a6e45914b7
Adam Bromiley <adam@bromiley.co.uk>
2020-06-20 22:20:37 +0100

    add coment

--- src/mandelbrot_parameters.c
+++ src/mandelbrot_parameters.c
@@ -1,4 +1,10 @@
 #include "mandelbrot_parameters.h"
 
 
-const double ESCAPE_RADIUS = 2.0;
+/* 
+ * Maximum magnitude before a number is considered to have escaped, assuming the
+ * calculation has already hit the maximum iteration count.
+ * The number is mathematically defined as 2, however a larger number allows for
+ * smoother colour mapping.
+ */
+const double ESCAPE_RADIUS = 256.0;
