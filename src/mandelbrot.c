3f3a1a9320334fd1a4a78f3f806259890e0a5929
Adam Bromiley <adam@bromiley.co.uk>
2020-06-30 15:36:16 +0100

    remove long double literals

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -661,8 +661,8 @@ ParseErr magnificationArgument(struct PlotCTX *parameters, char *argument, compl
             return PARSE_EERR;
     }
 
-    parameters->minimum = imageCentre - 0.5L * rangeDefault * powl(0.9L, magnification - 1.0L);
-    parameters->maximum = imageCentre + 0.5L * rangeDefault * powl(0.9L, magnification - 1.0L);
+    parameters->minimum = imageCentre - 0.5 * rangeDefault * pow(0.5, magnification - 1.0);
+    parameters->maximum = imageCentre + 0.5 * rangeDefault * pow(0.5, magnification - 1.0);
 
     return PARSE_SUCCESS;
 }
