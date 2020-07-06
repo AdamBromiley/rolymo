9e4ec8663411d673608edb6bea75a8795b797157
Adam Bromiley <adam@bromiley.co.uk>
2020-07-06 01:48:12 +0100

    add MP precision variables

--- include/ext_precision.h
+++ include/ext_precision.h
@@ -3,23 +3,41 @@
 
 
 #include <complex.h>
-#include <stdbool.h>
 
+#include <gmp.h>
+#include <mpc.h>
+
+
+typedef enum PrecisionMode
+{
+    STD_PRECISION,
+    EXT_PRECISION,
+    ARB_PRECISION
+} PrecisionMode;
 
 typedef union ExtDouble
 {
     double d;
     long double ld;
+    mpf_t mpd;
 } ExtDouble;
 
 typedef union ExtComplex
 {
     complex c;
     long double complex lc;
+    mpc_t mpc;
 } ExtComplex;
 
 
-extern bool extPrecision;
+extern const mpfr_prec_t ARB_PRECISION_BITS;
+
+extern const mpc_rnd_t ARB_CMPLX_ROUNDING;
+extern const mpfr_rnd_t ARB_REAL_ROUNDING;
+extern const mpfr_rnd_t ARB_IMAG_ROUNDING;
+
+
+extern PrecisionMode precision;
 
 
 #endif
