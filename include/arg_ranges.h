c2a4e4182ebf4ec1318523f6c37a55bab43a182a
Adam Bromiley <adam@bromiley.co.uk>
2020-07-10 02:10:50 +0100

    add dedicated file for arg ranges

---
+++ include/arg_ranges.h
@@ -0,0 +1,38 @@
+#ifndef ARG_RANGES_H
+#define ARG_RANGES_H
+
+
+#include <complex.h>
+#include <stddef.h>
+
+#include <mpfr.h>
+
+
+extern const complex COMPLEX_MIN;
+extern const complex COMPLEX_MAX;
+extern const long double complex COMPLEX_MIN_EXT;
+extern const long double complex COMPLEX_MAX_EXT;
+
+extern const complex C_MIN;
+extern const complex C_MAX;
+extern const long double complex C_MIN_EXT;
+extern const long double complex C_MAX_EXT;
+
+extern const double MAGNIFICATION_MIN;
+extern const double MAGNIFICATION_MAX;
+extern const long double MAGNIFICATION_MIN_EXT;
+extern const long double MAGNIFICATION_MAX_EXT;
+
+extern const unsigned long ITERATIONS_MIN;
+extern const unsigned long ITERATIONS_MAX;
+
+extern const size_t WIDTH_MIN;
+extern const size_t WIDTH_MAX;
+extern const size_t HEIGHT_MIN;
+extern const size_t HEIGHT_MAX;
+
+const mpfr_prec_t MP_BITS_MIN;
+const mpfr_prec_t MP_BITS_MAX;
+
+
+#endif
---
+++ src/arg_ranges.c
@@ -0,0 +1,47 @@
+#include "arg_ranges.h"
+
+#include <float.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <mpfr.h>
+
+
+/* Range of permissible complex numbers */
+const complex COMPLEX_MIN = -(DBL_MAX) - (DBL_MAX) * I;
+const complex COMPLEX_MAX = (DBL_MAX) + (DBL_MAX) * I;
+
+/* Range of permissible complex numbers (extended-precision) */
+const long double complex COMPLEX_MIN_EXT = -(LDBL_MAX) - (LDBL_MAX) * I;
+const long double complex COMPLEX_MAX_EXT = (LDBL_MAX) + (LDBL_MAX) * I;
+
+/* Range of permissible constant values */
+const complex C_MIN = -2.0 - 2.0 * I;
+const complex C_MAX = 2.0 + 2.0 * I;
+
+/* Range of permissible constant values (extended-precision) */
+const long double complex C_MIN_EXT = -2.0L - 2.0L * I;
+const long double complex C_MAX_EXT = 2.0L + 2.0L * I;
+
+/* Range of permissible magnification values */
+const double MAGNIFICATION_MIN = -(DBL_MAX);
+const double MAGNIFICATION_MAX = DBL_MAX;
+
+/* Range of permissible magnification values (extended-precision) */
+const long double MAGNIFICATION_MIN_EXT = -(LDBL_MAX);
+const long double MAGNIFICATION_MAX_EXT = LDBL_MAX;
+
+/* Range of permissible iteration counts */
+const unsigned long ITERATIONS_MIN = 0UL;
+const unsigned long ITERATIONS_MAX = ULONG_MAX;
+
+/* Range of permissible dimensions */
+const size_t WIDTH_MIN = 1;
+const size_t WIDTH_MAX = SIZE_MAX;
+const size_t HEIGHT_MIN = 1;
+const size_t HEIGHT_MAX = SIZE_MAX;
+
+/* Range of permissible precisions (arbitrary-precision) */
+const mpfr_prec_t MP_BITS_MIN = 1;
+const mpfr_prec_t MP_BITS_MAX = 16384;
