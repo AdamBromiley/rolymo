f1931402f9f4b0d191aafd91313ede97c914cf07
Adam Bromiley <adam@bromiley.co.uk>
2020-11-26 19:25:29 +0000

    change comment

--- src/mandelbrot_parameters.c
+++ src/mandelbrot_parameters.c
@@ -1,11 +1,10 @@
 #include "mandelbrot_parameters.h"
 
 
-/* 
- * Maximum magnitude before a number is considered to have escaped, assuming the
- * calculation has already hit the maximum iteration count.
- * The number is mathematically defined as 2, however a larger number allows for
- * smoother colour mapping.
+/* Maximum magnitude before a number is considered to have escaped, assuming the
+ * calculation has already hit the maximum iteration count. The number is
+ * mathematically defined as 2, however a larger number allows for smoother
+ * colour mapping.
  */
 const double ESCAPE_RADIUS = 256.0;
 const long double ESCAPE_RADIUS_EXT = 256.0L;
